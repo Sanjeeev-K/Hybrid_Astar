b8359143ee32d39d5fa1594eead1c0ea9d995435
Sanjeeev-K <sanjeevcannon@gmail.com>
2020-05-05 09:25:12 -0400

    Revision 2.0

--- testing.cpp
+++ testing.cpp
@@ -1,70 +1,239 @@
 #include<iostream>
 #include<vector>
 #include <math.h>
+#include<queue>
 #include "matplotlibcpp.h"
 #include "hybrid_breadth_first.h"
 namespace plt = matplotlibcpp;
 using namespace std;
+#define PI 3.14159265
+// #define PI 3.17
 
 double SPEED = 1.45;
 double LENGTH = 0.5;
+int num_theta = 4;
 
-void plot(double x1, double y1, double theta, double x2, double y2, double theta2){
-    double x_prev = x1;
-    double y_prev = y1;
-    double x_next = x_prev;
-    double y_next = y_prev;
-    for(int i = 0; i<=100; i++){
-        x_next = x_prev + (SPEED * cos(theta)  )*1/100;
-        y_next = y_prev + (SPEED * sin(theta) ) *1/100;
-        vector<double> vec_x = {x_next,x_prev};
-        vector<double> vec_y = {y_next, y_prev};
-        plt::plot(vec_x,vec_y);
-        x_prev = x_next;
-        y_prev = y_next;
+// void plot(double x1, double y1, double theta, double x2, double y2, double theta2){
+//     double x_prev = x1;
+//     double y_prev = y1;
+//     double x_next = x_prev;
+//     double y_next = y_prev;
+//     for(int i = 0; i<=100; i++){
+//         x_next = x_prev + (SPEED * cos(theta)  )*1/100;
+//         y_next = y_prev + (SPEED * sin(theta) ) *1/100;
+//         vector<double> vec_x = {x_next,x_prev};
+//         vector<double> vec_y = {y_next, y_prev};
+//         plt::plot(vec_x,vec_y);
+//         x_prev = x_next;
+//         y_prev = y_next;
         
-        // delta2 = delta * i/100
-        // double omega = SPEED / LENGTH * tan(delta2);
-        // theta = theta + omega;
+//         // delta2 = delta * i/100
+//         // double omega = SPEED / LENGTH * tan(delta2);
+//         // theta = theta + omega;
 
-        theta += (theta2 - theta)*1/100;
+//         theta += (theta2 - theta)*1/100;
 
 
 
-        if(theta > 2*3.1415) {  
-            theta = theta - 2*3.1415;
-        }
-    }
-    cout<<endl<<"Next x is :"<<x_next;
-    cout<<endl<<"Next y is :"<<y_next<<endl;
-    plt::ylim(-1,11);
-    plt::xlim(-1,11);
-    plt::show();
+//         if(theta > 2*3.1415) {  
+//             theta = theta - 2*3.1415;
+//         }
+//     }
+//     cout<<endl<<"Next x is :"<<x_next;
+//     cout<<endl<<"Next y is :"<<y_next<<endl;
+//     plt::ylim(-1,11);
+//     plt::xlim(-1,11);
+    // plt::show();
+// }
+
+void plot(double x, double y){
+    std::vector<double> x1, y1;
+    x1.push_back(x);
+    y1.push_back(y);
+    plt::plot(x1,y1,"rx");
 }
 
+// Sets up maze grid
+int X = 1;
+int _ = 0;
+
+vector<vector<int>> GRID = {
+  {_,X,X,_,_,_,_,_,_,_,X,X,_,_,_,_,},
+  {_,X,X,_,_,_,_,_,_,X,X,_,_,_,_,_,},
+  {_,X,X,_,_,_,_,_,X,X,_,_,_,_,_,_,},
+  {_,X,X,_,_,_,_,X,X,_,_,_,X,X,X,_,},
+  {_,X,X,_,_,_,X,X,_,_,_,X,X,X,_,_,},
+  {_,X,X,_,_,X,X,_,_,_,X,X,X,_,_,_,},
+  {_,X,X,_,X,X,_,_,_,X,X,X,_,_,_,_,},
+  {_,X,X,X,X,_,_,_,X,X,X,_,_,_,_,_,},
+  {_,X,X,X,_,_,_,X,X,X,_,_,_,_,_,_,},
+  {_,X,X,_,_,_,X,X,X,_,_,X,X,X,X,X,},
+  {_,X,_,_,_,X,X,X,_,_,X,X,X,X,X,X,},
+  {_,_,_,_,X,X,X,_,_,X,X,X,X,X,X,X,},
+  {_,_,_,X,X,X,_,_,X,X,X,X,X,X,X,X,},
+  {_,_,X,X,X,_,_,X,X,X,X,X,X,X,X,X,},
+  {_,X,X,X,_,_,_,_,_,_,_,_,_,_,_,_,},
+  {X,X,X,_,_,_,_,_,_,_,_,_,_,_,_,_,}};
+
+vector<double> START = {0.0,0.0,0.0};
+vector<int> GOAL = {(int)GRID.size()-1, (int)GRID[0].size()-1};
+
+class Element{
+    public:
+        double x;
+        double y;
+        double theta;
+        double f;
+        double cost;
+    Element(double xx, double yy, double t1){
+        x = xx; y = yy; theta = t1;f = 0; cost = 0;
+    }
+    Element(){
+        x = 0.0; y = 0.0; theta = 0.0; f = 0; cost = 0;
+    }
+};
+
+bool operator>(Element const & a, Element const & b)
+{
+    return a.f > b.f;
+}
+
+vector<Element> add_neighbors(Element current){
+    vector<Element> res;
+    double x_current = current.x;
+    double y_current = current.y;
+    double theta_current = current.theta;
+    for(double delta = -35; delta<40; delta += 35){
+        double x = x_current + SPEED * cos(theta_current);
+        double y = y_current + SPEED * sin(theta_current);
+        double delta_in_radians = (delta*PI)/180.0;
+        double omega = (SPEED / LENGTH) * tan(delta_in_radians);
+        double theta = theta_current + omega;
+        Element new_ele(x,y,theta);
+        res.push_back(new_ele);
+    }
+    return res;
+}
+
+
 int main(){
-    cout<<"Testing124124";
-    // vector<int> a{1,2,3};
-    // vector<int> b{1,2,4};
-    // plt::plot({1,3,2,4});
-    // plt::plot(a,b);
-    // plt::show();
-    
-    double theta = 0;
-    vector<int> start = {0,0};
-    double x = start[0];
-    double y = start[1];
-    
-    double next_x = x + SPEED * cos(theta);
-    double next_y = y + SPEED * sin(theta);
-    
-    double delta_in_degrees = -35;
-    double delta = delta_in_degrees * 3.1415 / 180;
-    double omega = SPEED / LENGTH * tan(delta);
-    double next_theta = theta + omega;
 
-    cout<<endl<<"Next x is :"<<next_x;
-    cout<<endl<<"Next y is :"<<next_y<<endl;
+    bool reached_goal = false;
 
-    plot(x,y,theta, next_x, next_y, next_theta);
+    // vector<int> start = {0,0};
+    // double theta = 0;
+
+
+    // double x = start[0];
+    // double y = start[1];
+    
+    // double next_x = x + SPEED * cos(theta);
+    // double next_y = y + SPEED * sin(theta);
+    
+    // double delta_in_degrees = -35;
+    // double delta = delta_in_degrees * 3.1415 / 180;
+    // double omega = SPEED / LENGTH * tan(delta);
+    // double next_theta = theta + omega;
+
+    // cout<<endl<<"Next x is :"<<next_x;
+    // cout<<endl<<"Next y is :"<<next_y<<endl;
+
+    // 1. Identify parent for current node. Update Open, Closed, Parent. Add Current to Open.
+    // Create Open, Closed, Explored.
+    // vector<vector<int> > a( m, vector<n,0> )
+
+    vector< vector < vector <int> > > explored ( num_theta, vector<vector<int>> ( GRID.size(), vector<int> ( GRID[0].size() , 0) ));
+    // vector<vector< vector<Element>>> a(num_theta, vector<vector<Element>>   (          (GRID.size(), vector<Element> (GRID[0].size()))            );
+    vector<Element> a(GRID[0].size());
+    vector<vector<Element>> b (GRID.size(),a);
+    vector<vector<vector<Element>>> parent (num_theta,b);
+
+    Element start(START[0],START[1],START[2]);
+    parent[0][0][0] = start;
+    // vector< vector < vector <Element> > > parent ( num_theta, vector<vector<int>> ( GRID.size(), vector<Element> ( GRID[0].size())));
+
+
+    //2. Create a Priority Queue. ASecinding order. 
+    // priority_queue<Element> open_list;
+    // vector<Element> closed_list;
+
+    priority_queue< Element, vector<Element>, greater<Element> > open_list;
+    // start.f = 100;
+    // Element apple(START[0],START[1],START[2]);
+    // apple.f = 110;
+    open_list.push(start);
+    // open_list.push(apple);
+    // while(!open_list.empty()){
+        // cout<<open_list.top().f<<" ";
+        // open_list.pop();
+    // }
+
+    while(!open_list.empty()){
+        Element current = open_list.top();
+        open_list.pop();
+        double x = current.x;
+        double y = current.y;
+        double theta = current.theta;
+        if(int(x)==GOAL[0] && int(y)==GOAL[1]){
+            cout<<"Reached Goal";
+            reached_goal = true;
+            break;
+        }
+        else{
+            vector<Element> neighbors;
+            neighbors = add_neighbors(current);
+            // for(auto j:neighbors){
+            //     cout<<j.x<<" "<<j.y<<" "<<j.theta<<" "<<j.cost<<" "<<j.f<<endl;
+            // }
+            // cout<<double(PI);
+            //check if neighbors are valid. //check if neighbors are unexplored.
+            for(auto i:neighbors){
+                if(i.x>=0 && i.x<=GRID.size() && i.y>0 && i.y<=GRID[0].size()){
+                    double theta_neighbor = i.theta;
+                    //Normalizing theta ..so that it lies in [0,2pi)
+                    while(theta_neighbor<2*PI){
+                        theta_neighbor += 2*PI;
+                    }
+                    int theta_neighbor_id = floor( (theta_neighbor*num_theta)/(2*PI) );
+                    if(explored[theta_neighbor_id][int(i.x)][int(i.y)]==0){
+                        //Heiristic - Euclidian distance 
+                        double h = sqrt( pow(i.x - x,2) + pow(i.y - y,2) );
+                        double c = i.cost + 1;
+                        double f = h+c;
+                        parent[theta_neighbor_id][int(i.x)][int(i.y)] = current;
+                        i.cost = c;
+                        i.f = f;
+                        open_list.push(i);
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    if(reached_goal==false){
+        cout<<"Path to Goal not found";
+    }
+
+    
+
+
+
+    // While Open List isn't empty
+
+        // Current = ... . Remove Current from Open. 
+
+        // If Current = Goal. Exit. 
+
+        // Obtain neighbors of Current.
+
+        // Check if Neighbors are valid. Check if Neighbors are unexplored. 
+            // If yes, Calculatute their Heuristic, Cost, Parent node and then Add them to Open List.
+
+        // Sort the list? (Else, use a priority queue)
+
+        
+
+    // plot(x,y,theta, next_x, next_y, next_theta);
 }
